bb364006caecd055ecf17f2d17e6b72d3f65ef7b
GustavoDias496 <88061007+GustavoDias496@users.noreply.github.com>
2022-08-24 23:08:19 -0300

    Criação model

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express"
+import productModel from "../database/productModel"
 
 const ProductsController = {
 
     async index(req:Request, res:Response): Promise<Response>{
-        let products = {}
+
+        let products = await productModel.find()
         return res.json(products)
     }
 }

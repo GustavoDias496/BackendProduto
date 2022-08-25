a9a933a5062f84a093d020cd8c5a816d3fdc9356
GustavoDias496 <88061007+GustavoDias496@users.noreply.github.com>
2022-08-24 22:36:36 -0300

    Conexão com o MongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
 import express, { Request, Response } from 'express'
+import mongoose from 'mongoose'
+require('dotenv/config')
 
+
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const SERVER_PORT = process.env.SERVER_PORT
 
 const app = express()
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.bpobqoi.mongodb.net/?retryWrites=true&w=majority`)
+.then((data)=>{
+    console.log('MongoDB Conectado com sucesso!')
+})
+.catch((erro) =>{
+    console.log('Erro ao conectar ao MongoDB', erro.message)
+})
 
 app.get('/', (req:Request, res:Response) =>{
     return res.send('Hello World!')
 })
 
-app.listen(5000)
+app.listen(`${SERVER_PORT}`)

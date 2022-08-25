8fd379c8d4c368b2ac26da4ae082923c7b48cc3d
GustavoDias496 <88061007+GustavoDias496@users.noreply.github.com>
2022-08-24 23:50:51 -0300

    Rotas e grud completos

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -3,11 +3,44 @@ import productModel from "../database/productModel"
 
 const ProductsController = {
 
-    async index(req:Request, res:Response): Promise<Response>{
+    async getAllProducts(req:Request, res:Response): Promise<Response>{
 
         let products = await productModel.find()
         return res.json(products)
-    }
+    },
+
+    async getOneProduct(req:Request, res:Response): Promise<Response>{
+        const {id} = req.params
+        let product = await productModel.findById(id)
+        return res.json(product)
+    },
+
+    async createProduct(req:Request, res:Response): Promise<Response>{
+        const { productName, value, amountSales, productCode, description} = req.body
+        let product = await productModel.create(req.body)
+        return res.json(product)
+    },
+
+    async updateProduct(req:Request, res:Response): Promise<Response>{
+        const {id} = req.params
+        
+        const { productName, value, amountSales, productCode, description} = req.body
+
+        let product = await productModel.findByIdAndUpdate(id, {
+            productName:productName,
+            value:value, 
+            amountSales:amountSales, 
+            productCode:productCode, 
+            description:description
+        })
+        return res.json(product)
+    },
+
+    async deleteProduct(req:Request, res:Response): Promise<Response>{
+        const {id} = req.params
+        let product = await productModel.findByIdAndDelete(id)
+        return res.json(product)
+    },
 }
 
 export default ProductsController
--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,10 @@ import ProductController from './controllers/productController'
 const router = Router()
 
 
-router.get('/products', ProductController.index)
-
+router.get('/products', ProductController.getAllProducts)
+router.get('/products/:id', ProductController.getOneProduct)
+router.post('/products', ProductController.createProduct)
+router.put('/products/:id', ProductController.updateProduct)
+router.delete('/products/:id', ProductController.deleteProduct)
 
 export default router
